5fa1bfc4897360efb79d124b9618afcf7ea7664f
freddycmh <fleddymok@gmail.com>
2025-07-14 12:17:27 -0400

    add production ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,25 +4,37 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-// 💡 Enable CORS first
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+// 💡 Enable CORS during development
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 // Middleware
 app.use(express.json());
-
-// 💡 Now apply the rate limiter
 app.use(rateLimiter);
 
 // Routes
 app.use("/api/notes", notesRoutes);
+
+// ✅ Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+
+//make it dynamic for production
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: BASE_URL,
 });
+
+
 
 export default api;
